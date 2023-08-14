58ccc92dff475c07b053b788bb5a75e23f75b002
iturres <emanueliturres.co@gmail.com>
2023-08-14 20:41:22 -0300

    feat: Add new 'animals' SELECTION queries to 'queries.sql'

--- queries.sql
+++ queries.sql
@@ -1,3 +1,58 @@
-/*Queries that provide answers to the questions from all projects.*/
+SELECT
+    animals.name
+FROM
+    animals
+WHERE
+    animals.name LIKE '%mon';
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT
+    animals.name
+FROM
+    animals
+WHERE
+    animals.date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+
+SELECT
+    animals.name
+FROM
+    animals
+WHERE
+    animals.neutered = TRUE
+    AND animals.escape_attempts < 3;
+
+SELECT
+    animals.date_of_birth
+FROM
+    animals
+WHERE
+    animals.name IN ('Agumon', 'Pikachu');
+
+SELECT
+    animals.name,
+    animals.escape_attempts
+FROM
+    animals
+WHERE
+    animals.weight_kg > 10.5;
+
+SELECT
+    *
+FROM
+    animals
+WHERE
+    animals.neutered = TRUE;
+
+SELECT
+    *
+FROM
+    animals
+WHERE
+    animals.name NOT IN ('Gabumon');
+
+SELECT
+    *
+FROM
+    animals
+WHERE
+    animals.weight_kg >= 10.4
+    AND animals.weight_kg <= 17.3;
