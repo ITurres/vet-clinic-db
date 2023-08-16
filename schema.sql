d2a1a37bc8d514e1721d44aa39791df79266b541
ITurres <emanueliturres.co@gmail.com>
2023-08-16 19:04:55 -0300

    feat: on 'schema.sql' Modify -animals- and Add new tables
    - Modified the -animals- table by:
        - Removing the 'species' column.
        - Adding both 'species_id' and 'owner_id' columns.
        - Adding a foreign key constraint to the 'species_id' column.
        - Adding a foreign key constraint to the 'owner_id' column.
    
    - Added two new tables -species- and -owners-
    
        - Both with a primary key column named 'id' and
          a column named 'name' for -species- / 'full_name' for -owners-.
        - owners- have 'age' column.

--- schema.sql
+++ schema.sql
@@ -6,5 +6,23 @@ CREATE TABLE
         escape_attempts SMALLINT NOT NULL,
         neutered BOOLEAN NOT NULL,
         weight_kg NUMERIC(10, 2) NOT NULL,
-        species VARCHAR(50) NULL
+        species_id INT,
+        owner_id INT,
+        CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE,
+        CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    species (
+        id INT GENERATED ALWAYS AS IDENTITY,
+        name VARCHAR(50) NOT NULL,
+        PRIMARY KEY (id)
+    );
+
+CREATE TABLE
+    owners (
+        id INT GENERATED ALWAYS AS IDENTITY,
+        full_name VARCHAR(50) NOT NULL,
+        age SMALLINT NOT NULL,
+        PRIMARY KEY (id)
     );
