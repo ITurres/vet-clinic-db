e3d8802ed7db269916514e638c15a62e69a29974
ITurres <emanueliturres.co@gmail.com>
2023-08-17 14:35:28 -0300

    feat: on 'schema.sql' Add new tables nad alter -animals- table
    - Altered -animals- table to add it's primary key referencer.
    - Added -vets- table.
    - Added -specializations- and -visits- junction tables.

--- schema.sql
+++ schema.sql
@@ -34,3 +34,41 @@ ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id) ON DE
 ALTER TABLE animals
 ADD COLUMN owner_id INT,
 ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE;
+
+CREATE TABLE
+    vets (
+        id INT GENERATED ALWAYS AS IDENTITY,
+        name VARCHAR(50) NOT NULL,
+        age SMALLINT NOT NULL,
+        date_of_graduation DATE NOT NULL,
+        PRIMARY KEY (id)
+    );
+
+CREATE TABLE
+    specializations (
+        species_id INT,
+        vet_id INT,
+        PRIMARY KEY (species_id, vet_id),
+        CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE,
+        CONSTRAINT fk_vet FOREIGN KEY (vet_id) REFERENCES vets (id) ON DELETE CASCADE
+    );
+
+--? animals was missing its primary key. 
+--? PostgreSQL won't automatically set the column id as the primary key when it's added as a foreign key.
+--! thus the ERROR: there is no unique constraint matching given keys for referenced table -animals-.
+--? the error will occur when trying to reference the table animals in another table.
+ALTER TABLE animals ADD PRIMARY KEY (id);
+
+--* then:
+CREATE TABLE
+    visits (
+        visits_id INT GENERATED ALWAYS AS IDENTITY,
+        animal_id INT,
+        vet_id INT,
+        date_of_visit DATE NOT NULL,
+        CONSTRAINT fk_animal FOREIGN KEY (animal_id) REFERENCES animals (id) ON DELETE CASCADE,
+        CONSTRAINT fk_vet FOREIGN KEY (vet_id) REFERENCES vets (id) ON DELETE CASCADE,
+        PRIMARY KEY (visits_id)
+    );
+
+--*
