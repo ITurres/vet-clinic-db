36c145c879cbeffb83d3b1444a7d80dadf81e669
ITurres <emanueliturres.co@gmail.com>
2023-08-17 11:56:36 -0300

    fix: on 'schema.sql' Remove/Add columns
    - Remove hardcoded foreign keys references and its columns from
      -animals- table.
    
    - Set 'species' column back.
    - Use ALTER to:
        - DROP the 'species' column.
        - ADD the 'species_id' and
          'owner_id' columns with their foreign keys references.

--- schema.sql
+++ schema.sql
@@ -6,10 +6,7 @@ CREATE TABLE
         escape_attempts SMALLINT NOT NULL,
         neutered BOOLEAN NOT NULL,
         weight_kg NUMERIC(10, 2) NOT NULL,
-        species_id INT,
-        owner_id INT,
-        CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE,
-        CONSTRAINT fk_owners FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE
+        species VARCHAR(50) NULL
     );
 
 CREATE TABLE
@@ -26,3 +23,14 @@ CREATE TABLE
         age SMALLINT NOT NULL,
         PRIMARY KEY (id)
     );
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT,
+ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT,
+ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners (id) ON DELETE CASCADE;
