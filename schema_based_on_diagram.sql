f66da2c31e44bf80d2400f9fb44a2d3c9f2653ba
Matt <mwbertolucci@gmail.com>
2023-08-22 17:49:55 -0400

    Add invoice and invoice_items tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -16,3 +16,23 @@ CREATE TABLE patients (
 ALTER TABLE medical_histories
 ADD CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES patients(id) ON DELETE CASCADE;
 
+CREATE TABLE invoices (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount DECIMAL(10,2) NOT NULL,
+  generated_at TIMESTAMP NOT NULL,
+  paid_at TIMESTAMP NOT NULL,
+  medical_history_id INT,
+  CONSTRAINT fk_medical_history_id FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id) ON DELETE CASCADE,
+  PRIMARY KEY (id)
+)
+
+CREATE TABLE invoice_items (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  unit_price DECIMAL(10,2) NOT NULL,
+  quantity INT NOT NULL,
+  total_price DECIMAL(10,2) NOT NULL,
+  invoice_id INT,
+  CONSTRAINT fk_invoice_id FOREIGN KEY (invoice_id) REFERENCES invoices(id) ON DELETE CASCADE,
+  treatment_id INT NOT NULL,
+  PRIMARY KEY (id)
+)
